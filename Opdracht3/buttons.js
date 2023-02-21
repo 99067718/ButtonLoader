249fd29ea0349ac2c4bdd5ca88083d9dbd9fee6d
Mielesgames <emieltlam11@gmail.com>
2023-02-21 09:22:38 +0100

    it works now

--- Opdracht3/buttons.js
+++ Opdracht3/buttons.js
@@ -1,3 +1,4 @@
+var colors = ["red", "purple", "blue", "black", "vanish"]
 while (true){
     var MainDiv = document.getElementById("container");
     var aantal = prompt("Hoeveel buttons wil jij hebben? (zeg 69 pls)");
@@ -22,7 +23,17 @@ for (i = 0; i< aantal; i++){
     var newButton = document.createElement("button");
     newButton.innerText = i + 1;
     newButton.style.backgroundColor = "green";
-    newButton.onclick = function() {this.style.backgroundColor = "red"};
+    newButton.value = 0
+    newButton.onclick = function() {
+        console.log(this.value)
+        if (colors[this.value] == "vanish"){
+            MainDiv.removeChild(this)
+        }
+        else{
+            this.style.backgroundColor = colors[this.value]
+            this.value = parseInt(this.value) + 1 
+        }
+};
     newButton.style.marginLeft = "20px";
     newButton.style.marginTop = "10px";
     newButton.style.marginBottom = "10px";
